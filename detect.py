24b864be77bf9d09784c92f56374441b54b74199
askarthikkumar <askarthikkumar@gmail.com>
2017-09-09 03:07:03 +0530

    Segmenting Images + Detecting + Recognising Faces
    Also, deletes the images on every pass.

--- detect.py
+++ detect.py
@@ -2,31 +2,92 @@ import os
 import sys
 import boto3
 import json
+import base64
 from PIL import Image
+import io
+import boto
+from io import BytesIO
+import time
+from boto.s3.key import Key
+
+
+
+
+def upload_photo(img):
+	conn = boto.connect_s3()
+	b = conn.get_bucket('croprvce')
+	k = Key(b)
+	k.key = img
+	k.set_contents_from_filename(img)
+
+#---------------------------------------------------
+
+	
 
 if __name__ == '__main__':
 	client = boto3.client('rekognition')
+
 	bucket = 'bucket'
 	BUCKET_NAME = 'classimages'
-	KEY = 'kng.JPG'
+	#KEY = 'kng.JPG'
+	
+	#bucket = conn.get_bucket('classimages')
 	s3 = boto3.resource('s3')
+
+
+	classBucket = s3.Bucket(BUCKET_NAME)
+	for object in classBucket.objects.all():
+		KEY = str(object.key)
+		print(type(KEY))
+
+
+
 	try:
 		s3.Bucket(BUCKET_NAME).download_file(KEY, 'local_img.jpg')
 	except botocore.exceptions.CLientError as e:
 		if e.response['Error']['Code'] == '404':
-			print("Object does'nt exist")
+			print("Object doesn't exist")
 		else:
-			raise
+			print("Else error")
 
-	response = client.detect_faces(Image = {"S3Object":{"Bucket":"classimages","Name":"kng.JPG"}}, Attributes = ['ALL'] )
+	response = client.detect_faces(Image = {"S3Object":{"Bucket":"studentfacervce","Name":KEY}}, Attributes = ['ALL'] )
+	print(response)
 
+	#byteArray = io.BytesIO()
+	index = 1
 	for face in response["FaceDetails"]:
-		img = Image.open("local_img.jpg")
+		img = Image.open("local_img.jpg", mode = 'r')
 		width, height = img.size
+
 		left = face["BoundingBox"]["Left"]*width
 		upper = face["BoundingBox"]["Top"]*height
 		right = left + face["BoundingBox"]["Width"]*width
 		lower = upper + face["BoundingBox"]["Height"]*height
 		img2 = img.crop((left, upper, right, lower))
-		img2.show()
 
+		img2.save('img' + str(index) + '.jpg')
+		
+		img2.show()
+		with open('img' + str(index) + '.jpg', 'rb') as image:
+			#image.show()
+			upload_photo('img' + str(index) + '.jpg')
+			response1 = client.search_faces_by_image(
+				CollectionId = 'stud-id',
+				Image = {
+					'S3Object' : {
+						'Bucket':'croprvce',
+						'Name':'img' + str(index) + '.jpg'
+					}
+				}
+			)
+			print(response1)
+			
+		os.system("aws s3 rm s3://croprvce --recursive")
+		os.system("rm "+'img' + str(index) + '.jpg')
+		index += 1
+
+		#os.system("aws s3 rm s3://classimages --recursive")
+	os.system("rm local_img.jpg")
+
+
+	
